3936b3fa1488e524bb8dd350bc65862f86788097
somaya224 <somaya224@yahoo.com>
2020-03-31 00:14:07 -0400

    created Controllers and Controller modules

--- src/components/layout/default/Template.js
+++ src/components/layout/default/Template.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import TopMenu from './TopMenu';
 import Dashboard from '../../pages/dashboard/Dashboard';
 import Logs from '../../pages/logs/Logs';
+import Controllers from '../../pages/controllers/Controllers';
 
 // Styles
 import './styles.scss'
@@ -19,6 +20,7 @@ function Template() {
         <Container className="container p-5 my-0 mx-auto bg-white border border-gray-400 mb-10">
             <Route exact path="/" component={Dashboard} />
             <Route path="/logs" component={Logs} />
+            <Route path="/controllers" component={Controllers} />
         </Container>
       </Container>
     </Router>
